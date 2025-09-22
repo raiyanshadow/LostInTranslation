467bafac6c72a9e7de5441c0c21d9ccaf8eb36b2
saks <angelachen1260@gmail.com>
2025-09-22 18:17:24 -0400

    code lowercase

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -42,8 +42,8 @@ public class CountryCodeConverter {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
                 // TODO Task B: use parts to populate the instance variables
-                countryCodeToCountry.put(parts[2], parts[0]);
-                countryToCountryCode.put(parts[0], parts[2]);
+                countryCodeToCountry.put(parts[2].toLowerCase(), parts[0]);
+                countryToCountryCode.put(parts[0], parts[2].toLowerCase());
             }
         }
         catch (IOException | URISyntaxException ex) {
